b7f07a64ccac319009b39cd4e3704d1e8526243d
Aditya Patil <adityapatil0446@gmail.com>
2025-04-16 19:14:25 +0530

    modifiy the add.py file

--- practice/add.py
+++ practice/add.py
@@ -1,2 +1,12 @@
 def addition(a,b):
     return a+b
+
+def subtract(a,b):
+    return a-b
+
+add = addition(10,20)
+print(f'addition : {add}')
+
+sub = subtract(30,10)
+print(f'subtraction : {sub}')
+
